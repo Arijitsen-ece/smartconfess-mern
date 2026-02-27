ed95db350150c99705bd6bae71995a5f2fb4df39
Arijitsen-ece <arijitsenarijitsen58@gmail.com>
2026-02-28 02:22:09 +0530

    Initial commit - SmartConfess Backend API

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "SmartConfess API Running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
